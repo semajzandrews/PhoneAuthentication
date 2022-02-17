1ba8b5c87b1d77e5c186d21b772a25bb29c02a6a
Semaj Andrews <semajzandrews@gmail.com>
2022-02-17 13:34:33 -0500

    Created PhoneAuth Project Files

---
+++ PhoneAuthentication/PhoneAuthentication/PhoneAuthenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhoneAuthenticationApp.swift
+//  PhoneAuthentication
+//
+//  Created by Semaj Andrews on 2/17/22.
+//
+
+import SwiftUI
+
+@main
+struct PhoneAuthenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
